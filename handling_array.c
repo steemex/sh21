b7304c6bff9df8e97a863d01ca6eedf79d8d773b
steemex <steemex@ya.ru>
2024-03-19 09:18:04 +0300

    Edited handling_array.c

--- handling_array.c
+++ handling_array.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 // gcc -Wall -Wextra -Werror handling_array.c
 
-int read_uder_input_digit();
+int read_user_input_digit();
 
 int main() {
     int ar[4];  // creating array of 4 int, array is not array, but pointer to first element
 
     for (int *p = ar; p-ar<4; p++) {
-        *p = read_uder_input_digit();
+        *p = read_user_input_digit();
         printf("*p = %d, ar = %p, p = %p\n", *p, ar, p);
         printf("digit value of p-ar = %ld\n", p-ar);
     }    
@@ -15,7 +15,7 @@ int main() {
     return 0;
 }
 
-int read_uder_input_digit() {
+int read_user_input_digit() {
     int d;
     
     printf("Enter integer: ");
