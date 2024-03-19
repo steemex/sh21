1c3962fe33370d76055353b7a6e893c5ceae412f
steemex <steemex@ya.ru>
2024-03-19 11:32:37 +0300

    Created sort.c

---
+++ T06D09-1-master/src/sort.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#define NMAX 5
+
+int read(int *arr); // returns 0 if OK, returns 1 in case of problem
+void sort();
+void print(int *arr);
+
+int main() {
+    int arr[NMAX];
+
+    if (read(arr) != 0) {
+        printf("n/a");
+        return 1;
+    }
+    
+//    sort(arr);
+    print(arr);
+
+    return 0;
+}
+
+int read(int *arr) {
+    char ch;
+    int num;
+
+//    for (int *p=arr; p-arr < NMAX; p++) {
+    for (int i = 0; i < NMAX; i++) {
+//        printf("iteration %ld\n", p-arr);
+//        if (scanf("%d", p) == 1 || (ch == '\n')) {
+        if (scanf("%d", &num) == 1 || (ch == '\n')) {
+            arr[i] = num;
+//            printf("ok\n");
+            // array element saved?
+        } else {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+void print(int *arr) {
+    for (int i=0; i<NMAX; i++) {
+        printf("%d ", arr[i]);
+    }
+}
